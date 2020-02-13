7afdb82125421cdc4ff7a0cf58f51d805118c393
Shijith m <35588484+shijithm7@users.noreply.github.com>
2020-02-13 22:22:00 +0530

    Update rating.py

--- rating.py
+++ rating.py
@@ -1,26 +1,30 @@
+#import required library
+
+
 
 from textblob import TextBlob
 import re
 
 
+#receving comment from user
 
 review=str(input("Enter your review : "))
 
-def clean_tweet(text): 
+def clean_textt(text): 
         return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", text).split()) 
 
 
 
-
+# generating rating value
 
 def get_rating_value(text): 
 
         # create TextBlob object of passed tweet text 
-        analysis = TextBlob(clean_tweet(text))
+        analysis = TextBlob(clean_textt(text))
         # set sentiment 
         return analysis.sentiment.polarity
 
-
+#function which scale rating in between 0-10
 def scale(x):
 
     OldRange = (10 - (-10))  
